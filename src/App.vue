4824c24731a7adbf0b40b499ec94d7c7f9eb14fc
wuyoufu <3318824790@qq.com>
2021-08-23 11:20:12 +0800

    完成了初始样式

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,27 @@
 <template>
-  <div>
-
+  <div class="todoapp">
+<todo-header></todo-header>
+<todo-main></todo-main>
+<todo-footer></todo-footer>
   </div>
 </template>
 
 <script>
+import TodoHeader from './components/TodoHeader.vue'
+import TodoMain from './components/TodoMain.vue'
+import TodoFooter from './components/TodoFooter.vue'
+
 export default {
   data () {
     return {
 
     }
   },
-
+components:{
+TodoMain,
+TodoHeader,
+TodoFooter
+},
   created () {
 
   },
